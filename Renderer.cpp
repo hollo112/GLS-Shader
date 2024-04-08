1334a256effbbf7b5e2abb258f1b899789249dca
Kwon Yong Jin <rnjsdydwls112@gmail.com>
2024-04-08 23:13:13 +0900

    CircleShape, CircleShapeCycle, HeartShapeCycle, Velocity Fixed

--- Renderer.cpp
+++ Renderer.cpp
@@ -20,6 +20,7 @@ void Renderer::Initialize(int windowSizeX, int windowSizeY)
 	//Load shaders
 	m_SolidRectShader = CompileShaders("./Shaders/SolidRect.vs", "./Shaders/SolidRect.fs");
 	m_ParticleShader = CompileShaders("./Shaders/Particle.vs", "./Shaders/Particle.fs");	// 파티클 쉐이더 사용
+	m_ParticleCloudShader = CompileShaders("./Shaders/ParticleCloud.vs", "./Shaders/ParticleCloud.fs");
 
 	//Create VBOs
 	CreateVertexBufferObjects();
@@ -222,37 +223,113 @@ void Renderer::CreateParticleCloud(int numParticles)
 	float centerX, centerY;
 	centerX = 0.f;
 	centerY = 0.f;
-	float size = 0.01;
+	float size = 0.005;
 	int particleCount = numParticles;
 	int vertexCount = particleCount * 6;
-	int floatCount = vertexCount * 3;
+	int floatCount = vertexCount * (3 + 1 + 3 + 1 + 1 + 1 + 1);								
+	// x, y, z, startTime, Vx, Vy, Vz, lifeTime, amp, period, value
+	float VX, VY, YZ;
+	float lifeTime;
+	float amp, period;
+	float value;
 
 	float* vertices = NULL;
 	vertices = new float[floatCount];
 
 	int index = 0;
 	for (int i = 0; i < particleCount; ++i) {
-		centerX = ((float)rand() / (float)RAND_MAX) * 2.f - 1.f;		// -1 ~ 1
-		centerY = ((float)rand() / (float)RAND_MAX) * 2.f - 1.f;		// -1 ~ 1
-		vertices[index] = centerX - size; ++index;
-		vertices[index] = centerY - size; ++index;
-		vertices[index] = 0.f; ++index;
-		vertices[index] = centerX + size; ++index;
-		vertices[index] = centerY + size; ++index;
-		vertices[index] = 0.f; ++index;
-		vertices[index] = centerX - size; ++index;
-		vertices[index] = centerY + size; ++index;
-		vertices[index] = 0.f; ++index;	//triangle1
+		float velocityScale = 0.2f;
+		//centerX = ((float)rand() / (float)RAND_MAX) * 2.f - 1.f;		// -1 ~ 1
+		//centerY = ((float)rand() / (float)RAND_MAX) * 2.f - 1.f;		// -1 ~ 1
+		centerX = 0;
+		centerY = 0;
+		VX = 0.f; //(((float)rand() / (float)RAND_MAX) * 2.f - 1.f) * velocityScale;
+		VY = -0.01f; //(((float)rand() / (float)RAND_MAX) * 2.f - 1.f) * velocityScale;
+		YZ = (((float)rand() / (float)RAND_MAX) * 2.f - 1.f) * velocityScale;
+		//VX = 1.f;
+		//VY = 0.5f;
+		//YZ = 0.f;
+		float StartTime = 8.f * ((float)rand() / (float)RAND_MAX) * 2.f - 1.f;
+		//lifeTime = 8.f * ((float)rand() / (float)RAND_MAX) + 1.f;
+		lifeTime = 1.f * ((float)rand() / (float)RAND_MAX) + 1.f;
+		//lifeTime = 2.f;
+		amp = (((float)rand() / (float)RAND_MAX) - 0.5f) * 2.f;
+		period = ((float)rand() / (float)RAND_MAX);
+		value = ((float)rand() / (float)RAND_MAX);
 
 		vertices[index] = centerX - size; ++index;
 		vertices[index] = centerY - size; ++index;
 		vertices[index] = 0.f; ++index;
+		vertices[index] = StartTime; ++index;
+		vertices[index] = VX; ++index;
+		vertices[index] = VY; ++index;
+		vertices[index] = YZ; ++index;
+		vertices[index] = lifeTime; ++index;
+		vertices[index] = amp; ++index;
+		vertices[index] = period; ++index;
+		vertices[index] = value; ++index;
+
+		vertices[index] = centerX + size; ++index;
+		vertices[index] = centerY + size; ++index;
+		vertices[index] = 0.f; ++index;
+		vertices[index] = StartTime; ++index;
+		vertices[index] = VX; ++index;
+		vertices[index] = VY; ++index;
+		vertices[index] = YZ; ++index;
+		vertices[index] = lifeTime; ++index;
+		vertices[index] = amp; ++index;
+		vertices[index] = period; ++index;
+		vertices[index] = value; ++index;
+
+		vertices[index] = centerX - size; ++index;
+		vertices[index] = centerY + size; ++index;
+		vertices[index] = 0.f; ++index;
+		vertices[index] = StartTime; ++index;
+		vertices[index] = VX; ++index;
+		vertices[index] = VY; ++index;
+		vertices[index] = YZ; ++index;	
+		vertices[index] = lifeTime; ++index;
+		vertices[index] = amp; ++index;
+		vertices[index] = period; ++index;
+		vertices[index] = value; ++index;
+		//triangle1
+
+		vertices[index] = centerX - size; ++index;
+		vertices[index] = centerY - size; ++index;
+		vertices[index] = 0.f; ++index;
+		vertices[index] = StartTime; ++index;
+		vertices[index] = VX; ++index;
+		vertices[index] = VY; ++index;
+		vertices[index] = YZ; ++index;
+		vertices[index] = lifeTime; ++index;
+		vertices[index] = amp; ++index;
+		vertices[index] = period; ++index;
+		vertices[index] = value; ++index;
+
 		vertices[index] = centerX + size; ++index;
 		vertices[index] = centerY - size; ++index;
 		vertices[index] = 0.f; ++index;
+		vertices[index] = StartTime; ++index;
+		vertices[index] = VX; ++index;
+		vertices[index] = VY; ++index;
+		vertices[index] = YZ; ++index;
+		vertices[index] = lifeTime; ++index;
+		vertices[index] = amp; ++index;
+		vertices[index] = period; ++index;
+		vertices[index] = value; ++index;
+
 		vertices[index] = centerX + size; ++index;
 		vertices[index] = centerY + size; ++index;
-		vertices[index] = 0.f; ++index;	//triangle2
+		vertices[index] = 0.f; ++index;
+		vertices[index] = StartTime; ++index;
+		vertices[index] = VX; ++index;
+		vertices[index] = VY; ++index;
+		vertices[index] = YZ; ++index;	
+		vertices[index] = lifeTime; ++index;
+		vertices[index] = amp; ++index;
+		vertices[index] = period; ++index;
+		vertices[index] = value; ++index;
+		//triangle2
 	}
 
 	glGenBuffers(1, &m_ParticleCloudVBO);
@@ -306,21 +383,55 @@ void Renderer::DrawParticle()
 void Renderer::DrawParticleCloud()
 {
 	//Program select
-	GLuint shader = m_ParticleShader;
-	glUseProgram(shader);															// 
+	GLuint shader = m_ParticleCloudShader;
+	glUseProgram(shader);			
+	GLuint stride = sizeof(float) * 11;
 
 	int ulTime = glGetUniformLocation(shader, "u_Time");
 	glUniform1f(ulTime, m_ParticleTime);
 	m_ParticleTime += 0.016;
 
 	int ulPeriod = glGetUniformLocation(shader, "u_Period");
-	glUniform1f(ulPeriod, 8.0);
+	glUniform1f(ulPeriod, 2.0);
+
+	int ulAcc = glGetUniformLocation(shader, "u_Acc");
+	glUniform2f(ulAcc, cos(m_ParticleTime), sin(m_ParticleTime));
 
 	int attribPosition = glGetAttribLocation(shader, "a_Position");
 	glEnableVertexAttribArray(attribPosition);													// attribPosition을 쓸것이다
-
 	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
-	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);			// 3-> float포인트의 개수, 0 -> GL_ARRAY_BUFFER의 시작점부터 읽어라, sizeof(float) * 3 -> 3으로 나눠짐
+	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, stride, 0);			// 3-> float포인트의 개수, 0 -> GL_ARRAY_BUFFER의 시작점부터 읽어라, sizeof(float) * 3 -> 3으로 나눠짐
+
+	int attribStartTime = glGetAttribLocation(shader, "a_StartTime");
+	glEnableVertexAttribArray(attribStartTime);
+	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
+	glVertexAttribPointer(attribStartTime, 1, GL_FLOAT, GL_FALSE, stride, (GLvoid*)(sizeof(float) * 3));		// 마지막 para -> 어디서부터 정보가 시작되는지.  x, y, z, startTime. startTime은 3번 건너 뛴다음이기 때문에
+
+	int attribVelocity = glGetAttribLocation(shader, "a_Velocity");
+	glEnableVertexAttribArray(attribVelocity);													
+	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
+	glVertexAttribPointer(attribVelocity, 3, GL_FLOAT, GL_FALSE, stride, (GLvoid*)(sizeof(float) * 4));
+
+	int attribLifeTime = glGetAttribLocation(shader, "a_LifeTime");
+	glEnableVertexAttribArray(attribLifeTime);
+	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
+	glVertexAttribPointer(attribLifeTime, 1, GL_FLOAT, GL_FALSE, stride, (GLvoid*)(sizeof(float) * 7));
+
+	int attribAmp = glGetAttribLocation(shader, "a_Amp");
+	glEnableVertexAttribArray(attribAmp);
+	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
+	glVertexAttribPointer(attribAmp, 1, GL_FLOAT, GL_FALSE, stride, (GLvoid*)(sizeof(float) * 8));
+
+	int attribPeriod = glGetAttribLocation(shader, "a_Period");
+	glEnableVertexAttribArray(attribPeriod);
+	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
+	glVertexAttribPointer(attribPeriod, 1, GL_FLOAT, GL_FALSE, stride, (GLvoid*)(sizeof(float) * 9));
+
+	int attribValue = glGetAttribLocation(shader, "a_Value");
+	glEnableVertexAttribArray(attribValue);
+	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
+	glVertexAttribPointer(attribValue, 1, GL_FLOAT, GL_FALSE, stride, (GLvoid*)(sizeof(float) * 10));
+	
 
 	glDrawArrays(GL_TRIANGLES, 0, m_ParticleCloudVertexCount);
 
---
+++ Shaders/ParticleCloud.vs
@@ -0,0 +1,192 @@
+#version 330
+
+in vec3 a_Position;
+in float a_StartTime;
+in vec3 a_Velocity;
+in float a_LifeTime;
+in float a_Amp;
+in float a_Period;
+in float a_Value;
+
+uniform float u_Time = 0;
+uniform	float u_Period = 2.0;
+uniform vec2 u_Acc = vec2(0,0);
+uniform vec2 u_AttractPos = vec2(0,0);
+
+const vec3 c_StartPos = vec3(-1, 0, 0);
+const vec3 c_Velocity = vec3(2.0, 2.0, 0);
+const vec3 c_ParaVelocity = vec3(2.0, 2.0, 0);
+const vec2 c_2DGravity = vec2(0.0, -0.9);
+const float c_PI = 3.141592;
+
+void Basic()
+{
+	vec4 newPosition = vec4(a_Position.xy * a_StartTime, 0, 1);
+	gl_Position = newPosition;
+}
+
+void Velocity()
+{
+	float t = u_Time - a_StartTime;		//새로운 시간
+	vec4 newPosition = vec4(a_Position, 1);
+	
+	if(t > 0)
+	{
+		t = a_LifeTime * fract(t / a_LifeTime);
+		float attractValue = fract(t / a_LifeTime);
+		float tt = t * t;
+		newPosition.xy = newPosition.xy + a_Velocity.xy * t + 0.5 * (c_2DGravity + u_Acc) * tt;
+		newPosition.xy = mix(newPosition.xy, u_AttractPos, attractValue);
+	}
+	else
+	{
+		newPosition.x = 10000000;
+	}
+
+	gl_Position = newPosition;
+}
+
+void Line()
+{
+	float newTime = abs(fract(u_Time / u_Period) - 0.5) * 2.0;
+	vec4 newPosition;
+	newPosition.xyz = (a_Position + c_StartPos) + c_Velocity * newTime;
+	newPosition.w = 1;
+	gl_Position = newPosition;
+}
+
+void Circle()
+{
+	float newTime = -fract(u_Time /  u_Period) * c_PI * 2.0;
+	vec2 trans = vec2(cos(newTime), sin(newTime));
+	vec4 newPosition;
+	newPosition.xy = a_Position.xy + trans;
+	newPosition.zw = vec2( 0, 1 );
+	gl_Position = newPosition;
+}
+
+void Parablola()
+{
+	float newTime = fract(u_Time /  u_Period);
+	float t = newTime;
+	float tt = t*t;
+	vec4 newPosition;
+	float transX = (a_Position.x + c_StartPos.x) + c_ParaVelocity.x * newTime + 0.5 * c_2DGravity.x * tt;
+	float transY = (a_Position.y + c_StartPos.y) + c_ParaVelocity.y * newTime + 0.5 * c_2DGravity.y * tt;
+	newPosition.xy = vec2(transX, transY);
+	newPosition.zw = vec2( 0, 1 );
+	gl_Position = newPosition;
+}
+
+void Triangle()
+{
+	
+}
+
+void CircleShape()
+{
+	vec4 newPosition = vec4(a_Position, 1);
+	float t = u_Time - a_StartTime;		//새로운 시간
+	float amp = a_Amp;
+	float period = a_Period;
+	
+	if(t > 0)
+	{
+		t = a_LifeTime * fract(t / a_LifeTime);
+		float tt = t * t;
+		float value = a_Value * 2.0 * c_PI;
+		float x = cos(value);
+		float y = sin(value);
+		newPosition.xy = newPosition.xy + vec2(x, y);
+		
+		vec2 newVel = a_Velocity.xy + c_2DGravity * t;
+		vec2 newDir = vec2(-newVel.y, newVel.x);	// 새로운 방향
+		newDir = normalize(newDir);
+		newPosition.xy = newPosition.xy + a_Velocity.xy * t + 0.5 * c_2DGravity * tt;
+		//newPosition.y = newPosition.y  + sin((newPosition.x + 1) * c_PI);
+		newPosition.xy = newPosition.xy  + newDir * (t * 0.1) * amp * sin(t* c_PI*period);			// t가 흐를수록 뒤에 더 증가
+	}
+	else
+	{
+		newPosition.x = 10000000;
+	}
+
+	gl_Position = newPosition;
+}
+
+void CircleShapeCycle()
+{
+	vec4 newPosition = vec4(a_Position, 1);
+	float t = u_Time - a_StartTime;		//새로운 시간
+	float amp = a_Amp;
+	float period = a_Period;
+	
+	if(t > 0)
+	{
+		t = a_LifeTime * fract(t / a_LifeTime);
+		float tt = t * t;
+		float value = a_StartTime * 2.0 * c_PI;
+		float x = cos(value);
+		float y = sin(value);
+		newPosition.xy = newPosition.xy + vec2(x, y);
+		
+		vec2 newVel = a_Velocity.xy + c_2DGravity * t;
+		vec2 newDir = vec2(-newVel.y, newVel.x);	// 새로운 방향
+		newDir = normalize(newDir);
+		newPosition.xy = newPosition.xy + a_Velocity.xy * t + 0.5 * c_2DGravity * tt;
+		//newPosition.y = newPosition.y  + sin((newPosition.x + 1) * c_PI);
+		newPosition.xy = newPosition.xy  + newDir * (t * 0.1) * amp * sin(t* c_PI*period);			// t가 흐를수록 뒤에 더 증가
+	}
+	else
+	{
+		newPosition.x = 10000000;
+	}
+
+	gl_Position = newPosition;
+}
+
+void HeartShapeCycle()
+{
+	vec4 newPosition = vec4(a_Position, 1);
+	float t = u_Time - a_StartTime;		//새로운 시간
+	float amp = a_Amp;
+	float period = a_Period;
+	
+	if(t > 0)
+	{
+		t = a_LifeTime * fract(t / a_LifeTime);
+		float tt = t * t;
+		float value = a_StartTime * 2.0 * c_PI;
+		float x = 16 * pow(sin(value), 3);
+		float y = 13 * cos(value) - 5 * cos(2 * value) - 2 * cos(3 * value) - cos(4 * value);
+		x *= 0.04;
+		y *= 0.04;
+		newPosition.xy = newPosition.xy + vec2(x, y);
+		
+		vec2 newVel = a_Velocity.xy + c_2DGravity * t;
+		vec2 newDir = vec2(-newVel.y, newVel.x);	// 새로운 방향
+		newDir = normalize(newDir);
+		newPosition.xy = newPosition.xy + a_Velocity.xy * t + 0.5 * c_2DGravity * tt;
+		//newPosition.y = newPosition.y  + sin((newPosition.x + 1) * c_PI);
+		newPosition.xy = newPosition.xy  + newDir * (t * 0.1) * amp * sin(t* c_PI*period);			// t가 흐를수록 뒤에 더 증가
+	}
+	else
+	{
+		newPosition.x = 10000000;
+	}
+
+	gl_Position = newPosition;
+}
+
+void main()
+{
+
+	//Line();
+	//Circle();
+	//Parablola();
+	//Basic();
+	//Velocity();
+	//CircleShape();
+	//CircleShapeCycle();
+	HeartShapeCycle();
+}
